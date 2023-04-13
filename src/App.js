01c829d7ff587bc7af3e520da6422ba90e81a96a
muhammadderic <muhammad.donny.e@gmail.com>
2023-04-13 15:10:16 +0700

    Create home page's content - container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import Register from "./pages/Register";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       {/* <Register /> */}
-      <Login />
+      {/* <Login /> */}
+      <Home />
     </>
   );
 }
