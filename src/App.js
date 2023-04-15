addfcefe5ade2e2b8e2b3287b6bd053284e44fe4
muhammadderic <muhammad.donny.e@gmail.com>
2023-04-15 18:40:53 +0700

    Adding protected route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 
 function App() {
   const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    }
+    return children
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={currentUser ? <Home /> : <Login />} />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        } />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
